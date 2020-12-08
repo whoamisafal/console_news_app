99606b488c39d9bcec95bbb72bd51267cb469f05
safalstha142 <60834166+safalstha142@users.noreply.github.com>
2020-12-08 21:13:56 +0545

    first commit

---
+++ news.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+newsApi="http://newsapi.org/v2/everything?q=bitcoin&from=2020-11-08&sortBy=publishedAt&apiKey=2625b85702544cf79cfa47f8b27070a0"
+
+
+
+class News:
+    def __init__(self,url):
+        self.url=url
+
+    def getNews(self):
+        news=[]
+        data=requests.get(self.url).text
+        jsondata=json.loads(data)# in dict format
+        for d in jsondata["articles"]:
+            da= Model(d["author"],d["title"],d["description"],d["urlToImage"],d["publishedAt"],d["url"])
+            news.append(da)
+            
+        return news
+
+
+
+class Model:
+    def __init__(self,author,title,description,imgUrl,publishedAt,resourceUrl):
+        self.author=author
+        self.title=title
+        self.description=description
+        self.imgUrl=imgUrl
+        self.publishedAt=publishedAt
+        self.resourceUrl=resourceUrl
+
+
+
+news= News(newsApi)
+d=news.getNews()
+
+for n in d:
+    print("author:\t"+(str)(n.author))
+    print("title:\t"+(str)(n.title))
+    print("Description:\t"+(str)(n.description))
+    print("Image url:\t"+(str)(n.imgUrl))
+    print("PublishAt:\t"+n.publishedAt)
+    print("ResourceUrl:\t"+n.resourceUrl)
+
